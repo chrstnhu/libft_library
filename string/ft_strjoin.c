a4525fced3917fb414d55b1f2debac0a16a4da86
Christine Hu <chrhu@paul-f3Ar9s1.clusters.42paris.fr>
2024-04-16 10:51:18 +0200

    add library

---
+++ string/ft_strjoin.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chrhu <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/12 13:17:03 by chrhu             #+#    #+#             */
+/*   Updated: 2023/11/19 11:55:55 by chrhu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/libft.h"
+
+// char	*ft_strjoin(char const *s1, char const *s2)
+// {
+// 	char	*dest;
+// 	size_t	i;
+// 	size_t	j;
+
+// 	i = 0;
+// 	j = 0;
+// 	dest = (char *)malloc (sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+// 	if (!dest)
+// 		return (0);
+// 	while (s1[j] != '\0')
+// 	{
+// 		dest[i] = s1[j];
+// 		i++;
+// 		j++;
+// 	}
+// 	j = 0;
+// 	while (s2[j] != '\0')
+// 	{
+// 		dest[i] = s2[j];
+// 		i++;
+// 		j++;
+// 	}
+// 	dest[i] = '\0';
+// 	return (dest);
+// }
+
+char	*ft_strjoin(char const *s1, char *s2)
+{
+	char	*ptr;
+	size_t	len_start;
+	size_t	len_buff;
+
+	if (!s1)
+	{
+		s1 = (char *)malloc(1);
+		if (!s1)
+			return (NULL);
+		s1[0] = '\0';
+	}
+	len_start = ft_strlen(s1);
+	len_buff = ft_strlen(s2);
+	ptr = (char *)malloc(len_start + len_buff + 1);
+	if (!ptr)
+	{
+		free(s1);
+		return (NULL);
+	}
+	ft_memcpy(ptr, s1, len_start);
+	ft_memcpy(ptr + len_start, buff, len_buff + 1);
+	ptr[len_start + len_buff] = '\0';
+	free(s1);
+	return (ptr);
+}
