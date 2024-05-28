f262a5d20b33d3bf71686817d39cdf5cbb04b84d
Christine Hu <chrhu@paul-f3Ar7s5.clusters.42paris.fr>
2024-05-28 11:59:18 +0200

    modified strjoin

--- string/ft_strjoin.c
+++ string/ft_strjoin.c
@@ -12,34 +12,35 @@
 
 #include "../includes/libft.h"
 
-// char	*ft_strjoin(char const *s1, char const *s2)
-// {
-// 	char	*dest;
-// 	size_t	i;
-// 	size_t	j;
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+ 	char	*dest;
+ 	size_t	i;
+ 	size_t	j;
 
-// 	i = 0;
-// 	j = 0;
-// 	dest = (char *)malloc (sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-// 	if (!dest)
-// 		return (0);
-// 	while (s1[j] != '\0')
-// 	{
-// 		dest[i] = s1[j];
-// 		i++;
-// 		j++;
-// 	}
-// 	j = 0;
-// 	while (s2[j] != '\0')
-// 	{
-// 		dest[i] = s2[j];
-// 		i++;
-// 		j++;
-// 	}
-// 	dest[i] = '\0';
-// 	return (dest);
-// }
+ 	i = 0;
+ 	j = 0;
+ 	dest = (char *)malloc (sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+ 	if (!dest)
+ 		return (0);
+ 	while (s1[j] != '\0')
+ 	{
+ 		dest[i] = s1[j];
+ 		i++;
+ 		j++;
+ 	}
+ 	j = 0;
+ 	while (s2[j] != '\0')
+ 	{
+ 		dest[i] = s2[j];
+ 		i++;
+ 		j++;
+ 	}
+ 	dest[i] = '\0';
+ 	return (dest);
+}
 
+/*
 char	*ft_strjoin(char const *s1, char *s2)
 {
 	char	*ptr;
@@ -67,3 +68,4 @@ char	*ft_strjoin(char const *s1, char *s2)
 	free(s1);
 	return (ptr);
 }
+*/
